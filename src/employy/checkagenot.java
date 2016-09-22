bd4f3d1a9e95a3f151f8970a0908dfebcd80ad65
cncoder <romennts@gmail.com>
2016-09-22 19:43:23 +0800

    add code1

--- src/employy/checkagenot.java
+++ src/employy/checkagenot.java
@@ -6,10 +6,21 @@ public class checkagenot {
 		 
 		Cl  clerk =new Cl("name", 11, 12);
 		System.out.println(clerk.age);
-	}
-	
-	
-		
-
-	
+	}	
 }
+
+class Cl {
+
+	public String nameString;
+	
+	public int age;
+	
+	private int salary;
+	
+	public Cl(String name,int age1,int salary1){
+		this.nameString=name;
+		this.age=age1;
+		this.salary=salary1;
+	}
+}
+
