15f873ce32b2bc5e02be50628c357a031a492290
cncoder <romennts@gmail.com>
2016-09-22 21:40:21 +0800

    lab1

--- src/lab2/cyc.java
+++ src/lab2/cyc.java
@@ -6,6 +6,8 @@ public class cyc {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Animal animal=new Dog();
+		animal.Cry();
 		Dog dog=new Dog();
 		dog.name="cc";
 		System.out.println(dog.name);
@@ -13,7 +15,13 @@ public class cyc {
 	}
 
 }
-
+/*
+ * 抽象类：父类的方法、功能不明确，所以就不会去实现
+ * 就使用abstract修饰。
+ * 继承了抽象类的子类一定要实现他所有抽象方法
+ * 抽象方法只能在之类中实现
+ * 抽象类不能直接被new出来，因为他自己的意义都不明确，编译器自然更加不明白
+ */
 class Animal{
 	public String name;
 	protected void Cry(){
