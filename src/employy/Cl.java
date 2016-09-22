59f5cc1a26957f2a0a408bff857b8e074d8b1a68
cncoder <romennts@gmail.com>
2016-09-22 19:41:51 +0800

    add code1

---
+++ src/employy/Cl.java
@@ -0,0 +1,17 @@
+package employy;
+
+public class Cl {
+
+		public String nameString;
+		
+		public int age;
+		
+		private int salary;
+		
+		public Cl(String name,int age1,int salary1){
+			this.nameString=name;
+			this.age=age1;
+			this.salary=salary1;
+		}
+	}
+	
---
+++ src/employy/checkagenot.java
@@ -0,0 +1,15 @@
+package employy;
+
+public class checkagenot {
+	
+	 public static void main(String[] args) {
+		 
+		Cl  clerk =new Cl("name", 11, 12);
+		System.out.println(clerk.age);
+	}
+	
+	
+		
+
+	
+}
